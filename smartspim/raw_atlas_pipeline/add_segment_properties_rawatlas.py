8f576aebf59f8cd34df3588a5cbad9e7448339e7
Austin Hoag <austinthomashoag@gmail.com>
2021-09-30 11:40:31 -0400

    update script to update segment properties

--- smartspim/raw_atlas_pipeline/add_segment_properties_rawatlas.py
+++ smartspim/raw_atlas_pipeline/add_segment_properties_rawatlas.py
@@ -6,34 +6,20 @@ from cloudvolume import CloudVolume
 from cloudvolume.lib import mkdir, touch
 import shutil
 import json
-
-src_seg_props_info = '/jukebox/LightSheetData/atlas/neuroglancer/atlas/princetonmouse/segment_properties/info'
+# for Princeton mouse atlas:
+#src_seg_props_info = '/jukebox/LightSheetData/atlas/neuroglancer/atlas/princetonmouse/segment_properties/info'
+# for allen atlas:
+src_seg_props_info = '/jukebox/LightSheetTransfer/atlas/neuroglancer/atlas/allenatlas_2017/segment_properties/info'
+# Set viz_dir to parent folder where you put your precomputed raw atlas layers
 viz_dir="/jukebox/LightSheetData/lightserv_testing/neuroglancer/jess_cfos" # bucket dir, so layers will be in $BUCKET/$DATASET/$LAYER 
 
+brains = [] # put list of brain ids here, e.g. "f37077_demonstrator"
 if __name__ == "__main__":
-    animal_dataset_dict = {
-        '201904_ymaze_cfos':
-            ['an4','an5','an6','an7','an8','an11',
-            'an12','an15','an16','an18','an19','an20',
-            'an22','an23','an24','an25','an26','an27'],
-        '201810_adultacutePC_ymaze_cfos':
-            ['dadult_pc_crus1_1','dadult_pc_crus1_2',
-            'dadult_pc_crus1_3','dadult_pc_crus1_4',
-            'dadult_pc_crus1_5'],
-        '202002_cfos':
-            ['an2_vecctrl_ymaze','an3_vecctrl_ymaze',
-             'an4_vecctrl_ymaze','an9_vecctrl_ymaze',
-             'an10_vecctrl_ymaze','an1_crus1_lat','an2_crus1_lat',
-             'an4_crus1_lat','an5_crus1_lat','an6_crus1_lat',
-             'an7_crus1_lat','an10_crus1_lat','an11_crus1_lat',
-             'an13_crus1_lat','an19_crus1_lat','an4_saline','an5_cno']
-    }
-    for dataset in animal_dataset_dict.keys():
-        animal_id_list = animal_dataset_dict[dataset]
-        for animal_id in animal_id_list:
+    brains = [] # put list of animal ids in here
+    for brain in brains:
             # Update the info file and save it
-            print(f"Dataset: {dataset}, animal_id: {animal_id}")
-            layer_dir = os.path.join(viz_dir,dataset,f'rawatlas_{animal_id}_iso')
+            print(f"Dataset: {dataset}, animal_id: {brain}")
+            layer_dir = os.path.join(viz_dir,f'{brain}_raw_atlas')
             vol = CloudVolume(f'file://{layer_dir}')
             info_dict = vol.info
             info_dict['segment_properties'] = "segment_properties"
